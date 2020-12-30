5af106f00d07ef6fb37c3937fca91b3510fb48b2
4teko7 <4teko7@gmail.com>
2020-12-30 11:32:24 +0300

    commentsWereCompleted

--- process.cpp
+++ process.cpp
@@ -3,11 +3,17 @@
  * @author Bilal Tekin (bilal.tekin@boun.edu.tr)
  * @brief The main idea of the project is to learn how to use namedpipe and communicate between parent and child processes. besides this, Checking the status of the children, and
  * doing necessary actions. There are 3 files which are executor.cpp, process.cpp, and watchdog.cpp.
- * Executor.cpp opens pipe and waits until watchdog writes the name and pids of the processes to the pipe. While watchdog writes this information,
+ * Executor.cpp opens pipe and waits until watchdog writes the name and pids of the processes to the pipe. Watchdog creates processes according to input file. While watchdog writes processes information,
  * executor reads from the pipe and do some actions according to instrucitons in the instruction file that is a parameter given to the executor.
- * There are many signal types in the instruction file. When signal for a specific process is activated by executor, that process receive signal and do some actions.
- * At the same time, watchdog watches the processes and when any process is terminated, it restart that process, and write the information of the newly created process to the pipe.
- * Then, executor reads that information from the pipe, and continue its job. 
+ * There are many signal types in the instruction file. When signal for a specific process is activated by executor, that process receive signal and do some actions. For example, when process takes SIGTERM
+ * signal, it exits from the program. Watchdog should wait Its children. It Watchdog does not wait for Its children, they will become Zombie, which is an unwanted situation.
+ * When a child is terminated, Watchdog will get the information about the terminated child thanks to wait function, and will restart the terminated child. For executor to continue its job, Watchdog should
+ * write the information about the newly created child to the pipe. Then, executor reads that information from the pipe, and continue its job. If Watchdog receives SIGTERM signal, then before terminating Watchdog,
+ * we should terminate all Its children. If we don't terminate children, they will become orphan and this is an unwanted situation. \n
+ * Explanation Of Code Structure: \n
+ * When process is created by Watchdog, it will create necessary signals and enter an infinite loop. When process gets any signal, then "mySignal" method will be executed. Inside this method, necessary actions are done such as
+ * output to a process output file, or exit.
+ * 
  * @version 0.1
  * @date 2020-12-30
  * 
--- watchdog.cpp
+++ watchdog.cpp
@@ -3,11 +3,20 @@
  * @author Bilal Tekin (bilal.tekin@boun.edu.tr)
  * @brief The main idea of the project is to learn how to use namedpipe and communicate between parent and child processes. besides this, Checking the status of the children, and
  * doing necessary actions. There are 3 files which are executor.cpp, process.cpp, and watchdog.cpp.
- * Executor.cpp opens pipe and waits until watchdog writes the name and pids of the processes to the pipe. While watchdog writes this information,
+ * Executor.cpp opens pipe and waits until watchdog writes the name and pids of the processes to the pipe. Watchdog creates processes according to input file. While watchdog writes processes information,
  * executor reads from the pipe and do some actions according to instrucitons in the instruction file that is a parameter given to the executor.
- * There are many signal types in the instruction file. When signal for a specific process is activated by executor, that process receive signal and do some actions.
- * At the same time, watchdog watches the processes and when any process is terminated, it restart that process, and write the information of the newly created process to the pipe.
- * Then, executor reads that information from the pipe, and continue its job. 
+ * There are many signal types in the instruction file. When signal for a specific process is activated by executor, that process receive signal and do some actions. For example, when process takes SIGTERM
+ * signal, it exits from the program. Watchdog should wait Its children. If Watchdog does not do this, Its children will become Zombie, which is an unwanted situation.
+ * When a child is terminated, Watchdog will get the information about the terminated child thanks to wait function, and will restart the terminated child. For executor to continue its job, Watchdog should
+ * write the information about the newly created child to the pipe. Then, executor reads that information from the pipe, and continue its job. If Watchdog receives SIGTERM signal, then before terminating Watchdog,
+ * we should terminate all Its children. If we don't terminate children, they will become orphan and this is an unwanted situation. \n
+ * Explanation Of The Code Structure: \n
+ * Watchdog opens the pipe, and writes its own information to the pipe.
+ * It calls 'createAllChildren' method to create Its all children. This method will create all children and write their information to the pipe.
+ * We will enter an infinite while loop inside main funciton of the watchdog, and via wait(NULL), we will wait for any child which is terminated, and do necessary actions.
+ * When any child is terminated, It is checked whether It is P1 or others. If the terminated child is P1, then all other children will be terminated thanks to "killAllChildren" method, and they will be created again by "createAllChildren" method .
+ * But If It is not, then only the terminated child will be created via "createOneChild" method.
+ * When Watchdog receives SIGTERM signal, then "killWatchdog" method will be executed. Before terminate watchdog, "killAllChildren" will be called and all Its children will be terminated before Watchdog terminates.
  * @version 0.1
  * @date 2020-12-30
  * 
@@ -29,7 +38,7 @@
 
 /*! amount of sleep ins second */
 unsigned int timeToSleep = 1; // Second
-void createAllChilds(int numberOfProcess, char * processOutput,bool isResultOfKill);
+void createAllChildren(int numberOfProcess, char * processOutput,bool isResultOfKill);
 void createOneChild(pid_t pidOfChild, int numberOfProcess, char * processOutput);
 void killAllChildren(bool shouldPOneBeKilled);
 void killWatchdog(int sigNumber);
@@ -83,7 +92,7 @@ int main(int argc, char *argv[]) {
 
     write(fd, processIdString.c_str(), 30); 
   
-    createAllChilds(numberOfProcess,processOutput,false);
+    createAllChildren(numberOfProcess,processOutput,false);
 
     signal(SIGTERM, killWatchdog); 
 
@@ -94,7 +103,7 @@ int main(int argc, char *argv[]) {
         
         if(pNumber == "P1"){ 
             killAllChildren(false); 
-            createAllChilds(numberOfProcess,processOutput,true); 
+            createAllChildren(numberOfProcess,processOutput,true); 
         } else {
             createOneChild(pidOfChild, numberOfProcess,processOutput); 
         }
@@ -151,7 +160,7 @@ void createOneChild(pid_t pidOfChild, int numberOfProcess, char * processOutput)
  * @param processOutput Output path of process file
  * @param isResultOfKill This variable is for writing output to file when a variable is terminated. At the beginning of the program, this is false, and we do not write anything to output file. 
  */
-void createAllChilds(int numberOfProcess, char * processOutput,bool isResultOfKill){
+void createAllChildren(int numberOfProcess, char * processOutput,bool isResultOfKill){
 
     stringstream processId; //Pids of Child
 
